03b0058e3a7191cf41ba3f33e4940fbff85a6b5a
Kenneth Lai <klai022@gmail.com>
2022-12-16 17:03:14 -0800

    Input field created and centered.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatButtonModule } from '@angular/material/button';
+import {MatFormFieldModule} from '@angular/material/form-field'
+import {FormsModule, ReactiveFormsModule} from '@angular/forms'
+import {MatSelectModule} from '@angular/material/select'
+import {HttpClientModule} from '@angular/common/http';
+import {MatNativeDateModule} from '@angular/material/core';
+import { MatInputModule } from '@angular/material/input';
+
 
 
 
@@ -15,7 +22,16 @@ import { MatSlideToggleModule } from '@angular/material/slide-toggle';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    MatSlideToggleModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatSelectModule,
+    HttpClientModule,
+    MatNativeDateModule,
+    MatInputModule,
+
+  
     
   ],
   providers: [],
