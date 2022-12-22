1df9ec9c8085826e45f69767dc6491ca631ed724
Kenneth Lai <klai022@gmail.com>
2022-12-21 21:22:22 -0800

    Implemented http post

--- src/app/control-f/control-f.component.ts
+++ src/app/control-f/control-f.component.ts
@@ -1,5 +1,9 @@
+import { HttpHeaders } from '@angular/common/http';
 import { Component, OnInit, Input, SimpleChanges } from '@angular/core';
 import { AlamedaWebAppService } from '../alameda-web-app.service';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import { HttpParams } from '@angular/common/http';
 
 
 
@@ -13,13 +17,24 @@ import { AlamedaWebAppService } from '../alameda-web-app.service';
 export class ControlFComponent implements OnInit{
 
   //@Input() entered: any;
-  entered:any;//aka search
-  filter:any;
+
+ 
+
+  entered:any;//aka search from home component input
+  filter:string='';//the entered string in the control-f component input field
   result:any;//again this needs to be in the results page component. using this here for testing
+  //returnedBleach:any;//from GetB() funcion in C#
+
+  sendBackend = {//prob delete
+    name: "Bleach String",
+    filter: this.filter
+  }
 
 
 
-  constructor(private alamedaWebAppService : AlamedaWebAppService){}
+
+
+  constructor(private alamedaWebAppService : AlamedaWebAppService, private http: HttpClient){}
 
   ngOnInit(): void{
     this.entered = localStorage.getItem('search');
@@ -30,7 +45,16 @@ export class ControlFComponent implements OnInit{
   }
 
   onSubmit()
-  {
+  {/*
+    this.http.post('https://localhost:7054/api/alameda_web_app_/UpdateBleach',{id: 0, name: this.filter}, 
+    this.httpOptions).subscribe(x=> console.log(x));*/
+
+    /*let bar: any = new Object();
+  bar.foo = 'Hello World';
+    */
+
+    this.http.post<any>('https://localhost:7054/api/alameda_web_app_', {code: this.filter}).subscribe(
+      x=> console.log(x));
 
   }
 
